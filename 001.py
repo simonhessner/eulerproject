b58118189262eec60e302a194b915547d42ffd5a
Simon Hessner <uldci@student.kit.edu>
2017-12-04 15:56:56 +0100

    Simplify 001.py by using lambda

--- 001.py
+++ 001.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-def calc_sum(n):
-	return sum([x for x in range(1, n) if x % 3 == 0 or x % 5 == 0])
+calc_sum = lambda n : sum([x for x in range(1, n) if x % 3 == 0 or x % 5 == 0])
 
-print(calc_sum(10))
+assert calc_sum(10) == 23
 print(calc_sum(1000))
